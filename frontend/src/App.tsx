fc2e2c6bcfa0572a6a5605d6c07d30766f79b58d
xen <aniketchatterjee.dev@gmail.com>
2024-10-31 16:50:44 +0530

    configured the frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button className="bg-orange hover:bg-hoverOrange">Click me</Button>
+    </>
+  );
+}
+
+export default App;
